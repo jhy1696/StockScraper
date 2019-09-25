eb80816f1f5ca0f68dce8638d2253d75bd2896ea
Harr <junha04@naver.com>
2019-09-25 15:06:16 +0900

    PER을 이용한 적정 가격 찾는 기능 추가

--- naverFinanceCrawler.py
+++ naverFinanceCrawler.py
@@ -3,7 +3,7 @@
 import requests, sys
 from bs4 import BeautifulSoup
 
-def getEPS(codeNumber):
+def getExpectEPS(codeNumber):
     url = "https://finance.naver.com/item/main.nhn?code=" + codeNumber
     result = requests.get(url)
     bs_obj = BeautifulSoup(result.content, "html.parser")
@@ -17,5 +17,70 @@ def getEPS(codeNumber):
     print(EPS)
     return EPS
 
-if (sys.argv[1] == "getEPS"):
-    getEPS(sys.argv[2])
+def getLastEPS(codeNumber):
+    url = "https://finance.naver.com/item/main.nhn?code=" + codeNumber
+    result = requests.get(url)
+    bs_obj = BeautifulSoup(result.content, "html.parser")
+
+    th_list = bs_obj.find(string="EPS(원)").parent.parent.parent.find_all("td") # 이상하게 find_parent는 안된다...
+    lastEPS = th_list[2].text.strip().replace(",", "")
+
+    companyName = bs_obj.find("div", {"class" : "wrap_company"}).find_next("h2").text
+    print(companyName)
+    print("last EPS")
+    print(lastEPS)
+    return lastEPS
+
+def getFiveYearPER(codeNumber):
+    url = "http://search.itooza.com/search.htm?seName=" + codeNumber
+    result = requests.get(url)
+    bs_obj = BeautifulSoup(result.content, "html.parser")
+
+    fiveYearSummary = bs_obj.find("table", {"summary": "5년치 주요 가치지표"}) # td 태그 중 class가 cell_strong 인 것들을 모두 찾는다
+    td_list = fiveYearSummary.find_all("td")
+    fiveYearPER = td_list[0].text
+
+    companyName = bs_obj.find("span", {"class" : "name"}).text
+
+    print(companyName)
+    print("5년 PER")
+    print(fiveYearPER)
+    return fiveYearPER
+
+def getReasonablePriceByPER(codeNumber):
+    expectEPS = getExpectEPS(codeNumber)
+    lastEPS = getLastEPS(codeNumber)
+    fiveYearPER = getFiveYearPER(codeNumber)
+    companyName = getCompanyName(codeNumber)
+
+    print(companyName)
+    print("적정 주가 : ")
+    print(float(lastEPS) * float(fiveYearPER))
+    print("~")
+    print(float(expectEPS) * float(fiveYearPER))
+    print("20% 더 싼 가격 : ")
+    print(float(lastEPS) * float(fiveYearPER) * 0.8)
+    print("~")
+    print(float(expectEPS) * float(fiveYearPER) * 0.8)
+
+
+def getCompanyName(codeNumber):
+    url = "https://finance.naver.com/item/main.nhn?code=" + codeNumber
+    result = requests.get(url)
+    bs_obj = BeautifulSoup(result.content, "html.parser")
+    companyName = bs_obj.find("div", {"class" : "wrap_company"}).find_next("h2").text
+    return companyName
+
+
+if len(sys.argv)-1 <= 1:
+    print("python naverFinanceCrawler getExpectEPS 123456")
+elif sys.argv[1] == "getExpectEPS":
+    getExpectEPS(sys.argv[2])
+elif sys.argv[1] == "getFiveYearPER":
+    getFiveYearPER(sys.argv[2])
+elif sys.argv[1] == "getLastEPS":
+    getLastEPS(sys.argv[2])
+elif sys.argv[1] == "getReasonablePriceByPER":
+    getReasonablePriceByPER(sys.argv[2])    
+else:
+    print("wrong parameter!") 
